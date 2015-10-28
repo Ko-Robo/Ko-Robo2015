c1bf53f78d52f041b4a7bff21c63997027023b15
KoiShin <koikeshinnosuke0415@gmail.com>
2015-10-28 22:24:55 +0900

    replace camel case to snake case

--- Robocup/koreisai2015/koreisai2015_reception/main.cpp
+++ Robocup/koreisai2015/koreisai2015_reception/main.cpp
@@ -2,7 +2,7 @@
 #include "motor.hpp"
 #include "kicker.hpp"
 
-#define harfOfMax 32768.0
+#define HAFT_OF_MAX 32768.0
 
 Serial pc(USBTX, USBRX);
 Serial xbee(PA_11, PA_12);
@@ -11,45 +11,45 @@ Motor  motorB(PC_2, PC_3, PB_7);
 Motor  motorC(PB_15, PB_14, PB_13);
 Motor  motorD(PC_5, PC_8, PC_6);
 
-char operationData[6];
+char operation_data[6];
 
 void receive(void) {
     static int i = 1;
-    operationData[i % 6] = xbee.getc();
+    operation_data[i % 6] = xbee.getc();
     i++;
 }
 
 int main(void) {
-    motorA.setLimit(0.3);
-    motorB.setLimit(0.33);
-    motorC.setLimit(0.3);
-    motorD.setLimit(0.3);
+    motorA.set_limit(0.3);
+    motorB.set_limit(0.33);
+    motorC.set_limit(0.3);
+    motorD.set_limit(0.3);
 
     xbee.format(8, Serial::None, 1);
     while (xbee.getc());
     xbee.attach(&receive, Serial::RxIrq);
 
     while (1) {
-        if (operationData[1] & 0b01) {
+        if (operation_data[1] & 0b01) {
             motorA.run(1);
             motorB.run(1);
             motorC.run(1);
             motorD.run(1);
-        } else if (operationData[1] & 0b10) {
+        } else if (operation_data[1] & 0b10) {
             motorA.run(-1);
             motorB.run(-1);
             motorC.run(-1);
             motorD.run(-1);
         } else {
-            unsigned short upToDown    = (operationData[2] << 8) | operationData[3];
-            unsigned short leftToRight = (operationData[4] << 8) | operationData[5];
-            double upToDownCoefficient = upToDown / harfOfMax - 1;
-            double leftToRightCoefficient = leftToRight / harfOfMax - 1;
+            unsigned short up_to_down    = (operation_data[2] << 8) | operation_data[3];
+            unsigned short left_to_right = (operation_data[4] << 8) | operation_data[5];
+            double up_to_down_coefficient = up_to_down / HAFT_OF_MAX - 1;
+            double left_to_right_coefficient = left_to_right / HAFT_OF_MAX - 1;
 
-            double powerA = upToDownCoefficient *  0.7 + leftToRightCoefficient *  0.7;
-            double powerB = upToDownCoefficient *  0.7 + leftToRightCoefficient * -0.7;
-            double powerC = upToDownCoefficient * -0.7 + leftToRightCoefficient * -0.7;
-            double powerD = upToDownCoefficient * -0.7 + leftToRightCoefficient *  0.7;
+            double powerA = up_to_down_coefficient *  0.7 + left_to_right_coefficient *  0.7;
+            double powerB = up_to_down_coefficient *  0.7 + left_to_right_coefficient * -0.7;
+            double powerC = up_to_down_coefficient * -0.7 + left_to_right_coefficient * -0.7;
+            double powerD = up_to_down_coefficient * -0.7 + left_to_right_coefficient *  0.7;
 
             motorA.run(powerA);
             motorB.run(powerB);
--- Robocup/koreisai2015/koreisai2015_transmission/main.cpp
+++ Robocup/koreisai2015/koreisai2015_transmission/main.cpp
@@ -1,8 +1,8 @@
 #include "mbed.h"
 
 Serial xbee(dp16, dp15);
-AnalogIn leftToRight(dp9);
-AnalogIn upToDown(dp10);
+AnalogIn left_to_right(dp9);
+AnalogIn up_to_down(dp10);
 BusIn buttons(dp4, dp5, dp6);
 BusOut leds(dp14,dp17,dp28);
 PwmOut pwm(dp18);
@@ -11,33 +11,33 @@ int main(void) {
 
     while (1) {
         xbee.putc(0);
-        wait_ms(15);   
+        wait_ms(15);
         pwm = 1;
-        unsigned short buttonsData = buttons;
-        xbee.putc(buttonsData);
+        unsigned short buttons_data = buttons;
+        xbee.putc(buttons_data);
         leds = buttons;
         wait_ms(15);
 
         pwm = 0.9;
-        unsigned short upToDownData = upToDown.read_u16();
-        unsigned short upperEightBits = upToDownData >> 8;
-        xbee.putc(upperEightBits);
+        unsigned short up_to_down_data = up_to_down.read_u16();
+        unsigned short upper_eight_bits = up_to_down_data >> 8;
+        xbee.putc(upper_eight_bits);
         wait_ms(15);
 
         pwm = 0.8;
-        unsigned short lowerEightBits = upToDownData;
-        xbee.putc(lowerEightBits);
+        unsigned short lower_eight_bits = up_to_down_data;
+        xbee.putc(lower_eight_bits);
         wait_ms(15);
 
         pwm = 0.7;
-        unsigned short leftToRightData = leftToRight.read_u16();
-        upperEightBits = leftToRightData >> 8;
-        xbee.putc(upperEightBits);
+        unsigned short left_to_right_data = left_to_right.read_u16();
+        upper_eight_bits = left_to_right_data >> 8;
+        xbee.putc(upper_eight_bits);
         wait_ms(15);
 
         pwm = 0.8;
-        lowerEightBits = leftToRightData;
-        xbee.putc(lowerEightBits);
+        lower_eight_bits = left_to_right_data;
+        xbee.putc(lower_eight_bits);
         wait_ms(15);
 
         pwm = 0.9;
