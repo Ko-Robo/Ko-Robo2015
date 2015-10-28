bc87a7709de79435ada884e5c1d1c8f4ef056793
KoiShin <koikeshinnosuke0415@gmail.com>
2015-10-29 08:41:03 +0900

    change buzzer 's pin to PWM

--- Robocup/main2016/voltage_current_limit/main.cpp
+++ Robocup/main2016/voltage_current_limit/main.cpp
@@ -5,7 +5,7 @@
 BusOut anode(dp1, dp2, dp4, dp6, dp9, dp10, dp11, dp13);
 BusOut cathode(dp15, dp16, dp17, dp18);
 
-DigitalOut buzzer(dp24);
+PwmOut buzzer(dp24);
 DigitalIn button(dp25);
 
 I2C communication(dp5, dp27);
