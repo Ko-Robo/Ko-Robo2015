6b2c4f34ae231c2c435b9179805432a30534109f
KoiShin <koikeshinnosuke0415@gmail.com>
2015-10-30 12:44:59 +0900

    create SevenSegmentLED class

--- Robocup/main2016/voltage_current_limit/main.cpp
+++ Robocup/main2016/voltage_current_limit/main.cpp
@@ -2,8 +2,7 @@
 #include "limit.hpp"
 #include "seven-segment-LED.hpp"
 
-BusOut anode(dp1, dp2, dp4, dp6, dp9, dp10, dp11, dp13);
-BusOut cathode(dp15, dp16, dp17, dp18);
+SevenSegmentLED seven_segment(dp1, dp2, dp4, dp6, dp9, dp10, dp11, dp13, dp15, dp16, dp17, dp18);
 
 PwmOut buzzer(dp24);
 DigitalIn button(dp25);
@@ -12,7 +11,10 @@ I2C communication(dp5, dp27);
 DigitalOut to_main(dp26);
 
 int main(void) {
+    seven_segment.enable_dynamic_lighting(2500);
+    int val = 0;
     while (1) {
-
+        seven_segment.display_data(val);
+        val++;
     }
 }
--- Robocup/main2016/voltage_current_limit/seven-segment-LED.hpp
+++ Robocup/main2016/voltage_current_limit/seven-segment-LED.hpp
@@ -2,7 +2,7 @@
  * @file   : seven-segment-LED.h (1.0)
  * @brief  : light 7seg
  * @author : Shinnosuke KOIKE
- * @date   : 2015/10/23
+ * @date   : 2015/10/29
  */
 
 #ifndef SEVEN_SEGMENT_LED_H
@@ -12,12 +12,87 @@
 
 class SevenSegmentLED {
 public:
-    SevenSegmentLED(/* ピン名(BusOutで一度に指定??) */);
-    light();
+    SevenSegmentLED(PinName anode1_, PinName anode2_, PinName anode3_,
+                    PinName anode4_, PinName anode5_, PinName anode6_,
+                    PinName anode7_, PinName anode8_,
+                    PinName cathode1_, PinName cathode2_,
+                    PinName cathode3_, PinName cathode4_);
+    void display_data(int value, int dot_point = 0);
+    void enable_dynamic_lighting(int interval);
 
 private:
+    BusOut anode;
+    BusOut cathode;
     Ticker timer;
-    char displaying_char[4];
+    char data[4];
+    void dynamic_lighting();
 };
+
+SevenSegmentLED::SevenSegmentLED(PinName anode1_, PinName anode2_, PinName anode3_,
+        PinName anode4_, PinName anode5_, PinName anode6_, PinName anode7_, PinName anode8_,
+        PinName cathode1_, PinName cathode2_, PinName cathode3_, PinName cathode4_) :
+        anode(anode1_, anode2_, anode3_, anode4_, anode5_, anode6_, anode7_, anode8_),
+        cathode(cathode1_, cathode2_, cathode3_, cathode4_) {
+    for (int i = 0; i < 4; i++) {
+        data[i] = 0;
+    }
+}
+
+void SevenSegmentLED::enable_dynamic_lighting(int interval) {
+    timer.attach_us(this, &SevenSegmentLED::dynamic_lighting, interval);
+}
+
+void SevenSegmentLED::display_data(int value, int dot_point) {
+    for (int i = 3; i > 0; value /= 10, i--) {
+        switch (value % 10) {
+            case 1:
+                data[i] = 0b01100000; // 1
+                break;
+            case 2:
+                data[i] = 0b11011010; // 2
+                break;
+            case 3:
+                data[i] = 0b11110010; // 3
+                break;
+            case 4:
+                data[i] = 0b01100110; // 4
+                break;
+            case 5:
+                data[i] = 0b10110110; // 5
+                break;
+            case 6:
+                data[i] = 0b10111110; // 6
+                break;
+            case 7:
+                data[i] = 0b11100000; // 7
+                break;
+            case 8:
+                data[i] = 0b11111110; // 8
+                break;
+            case 9:
+                data[i] = 0b11110110; // 9
+                break;
+            case 0:
+                data[i] = 0b11111100; // 0
+                break;
+            default:
+                data[i] = 0b00000000; // non
+                break;
+        }
+        if (3 - dot_point == 1) {
+            data[i] |= 1;
+        }
+    }
+}
+
+void SevenSegmentLED::dynamic_lighting() {
+    static unsigned int shift_num = 0;
+    cathode = ~(1 << shift_num);
+    anode = data[shift_num];
+    shift_num++;
+    if (shift_num == 4) {
+        shift_num = 0;
+    }
+}
 
 #endif /* SEVEN_SEGMENT_LED_H */
