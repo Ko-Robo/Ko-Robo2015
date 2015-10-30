8d7641e48b496d3ff430dea4ff2cb0b0fbc08b0f
KoiShin <koikeshinnosuke0415@gmail.com>
2015-10-30 22:36:29 +0900

    create MotorThreeWheeler class by Yehoy

--- Robocup/main2016/main/motor-three-wheeler.hpp
+++ Robocup/main2016/main/motor-three-wheeler.hpp
@@ -1,8 +1,8 @@
 /**
  * @file   : motor-three-wheeler.h (1.0)
  * @brief  : motors for robot
- * @author : Shinnosuke KOIKE
- * @date   : 2015/10/23
+ * @author : Yohei SAITO(, Shinnosuke KOIKE)
+ * @date   : 2015/10/30
  */
 
 #ifndef MOTOR_FOR_ROBOT_H
@@ -11,16 +11,18 @@
 #include "mbed.h"
 #include "motor.hpp"
 
+#define M_PI 3.14159265358979323846
+
 class MotorThreeWheeler {
 public:
     MotorThreeWheeler(Motor motor1_, Motor motor2_, Motor motor3_);
-    void turnClockwise();
-    void turnCounterclockwise();
-    void goStraight();
-    void goBack();
-    void goRight();
-    void goLeft();
-    void run();
+    void brake(void);
+    void turn(float power);
+    void go_straight(float power);
+    void go_back(float power);
+    void go_right(float power);
+    void go_left(float power);
+    void run(float power, float angle);
 
 private:
     Motor motor1;
@@ -30,6 +32,59 @@ private:
 
 MotorThreeWheeler::MotorThreeWheeler(Motor motor1_, Motor motor2_, Motor motor3_) :
     motor1(motor1_), motor2(motor2_), motor3(motor3_) {
+}
+
+void MotorThreeWheeler::brake(){
+    motor1.brake();
+    motor2.brake();
+    motor3.brake();
+}
+
+void MotorThreeWheeler::turn(float power){
+    motor1.run(power);
+    motor2.run(power);
+    motor3.run(power);
+}
+
+void MotorThreeWheeler::run(float power, float angle){
+    while (angle > (2 * M_PI)) {
+        angle -= (2 * M_PI);
+    }
+
+    double component_x = 0.7559 * cos(angle);
+    double component_y = 0.6546 * sin(angle);
+
+    motor1.run(power * ( component_x - component_y));
+    motor2.run(power * (-component_x - component_y));
+    motor3.run(power * component_y);
+}
+
+void MotorThreeWheeler::go_straight(float power){
+    power *= 0.7559;
+    motor1.run( power);
+    motor2.run(-power);
+    motor3.run(0);
+}
+
+void MotorThreeWheeler::go_back(float power){
+    power *= 0.7559;
+    motor1.run(-power);
+    motor2.run(power);
+    motor3.run(0);
+}
+
+void MotorThreeWheeler::go_right(float power){
+    power *= 0.6546;
+    motor1.run(-power);
+    motor2.run(-power);
+    motor2.run( power);
+}
+
+void MotorThreeWheeler::go_left(float power){
+    power *= 0.6546;
+    motor1.run( power);
+    motor2.run( power);
+    motor2.run(-power);
 }
 
 #endif /* MOTOR_FOR_ROBOT_H */
