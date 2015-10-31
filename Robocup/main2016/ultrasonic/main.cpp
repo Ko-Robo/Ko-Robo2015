47a603f21ea2e917a359febee7a9445a982bfad6
KoiShin <koikeshinnosuke0415@gmail.com>
2015-10-31 10:51:42 +0900

    create main algorithm of ultrasonic

--- Robocup/main2016/ultrasonic/main.cpp
+++ Robocup/main2016/ultrasonic/main.cpp
@@ -1,13 +1,20 @@
 #include "mbed.h"
-#include "hcsr04.h"
+#include "ultrasonic.hpp"
 
-I2C communication(dp5, dp27);
-HCSR04 hcsr04_1(dp1, dp2);
-HCSR04 hcsr04_2(dp4, dp6);
-HCSR04 hcsr04_3(dp9, dp10);
+Ultrasonic ultrasonic_1(dp1, dp2);
+Ultrasonic ultrasonic_2(dp4, dp6);
+Ultrasonic ultrasonic_3(dp9, dp10);
+
+I2CSlave communication(dp5, dp27);
 
 int main(void) {
+    char distances[3];
+    communication.address(0xF0);
     while (1) {
+        distances[0] = ultrasonic_1.measure_distance();
+        distances[1] = ultrasonic_2.measure_distance();
+        distances[2] = ultrasonic_3.measure_distance();
 
+        communication.write(distances, 1);
     }
 }
