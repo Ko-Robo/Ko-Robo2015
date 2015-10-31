55ce104f9428187269d0e2672c027589d6f3bcd2
KoiShin <koikeshinnosuke0415@gmail.com>
2015-10-31 10:25:31 +0900

    fix using class name

--- Robocup/main2016/controller/main.cpp
+++ Robocup/main2016/controller/main.cpp
@@ -1,6 +1,6 @@
 #include "mbed.h"
 
-I2C communication(dp5, dp27);
+I2CSlave communication(dp5, dp27);
 
 int main(void) {
     communication.address(0x32);
--- Robocup/main2016/infrared/main.cpp
+++ Robocup/main2016/infrared/main.cpp
@@ -3,7 +3,7 @@
 BusIn infrared(dp1, dp2, dp4, dp6, dp9, dp10, dp11, dp13, dp15, dp16, dp17, dp18);
 BusIn far(dp24, dp25);
 
-I2C communication(dp5, dp27);
+I2CSlave communication(dp5, dp27);
 
 int main(void) {
     while (1) {
