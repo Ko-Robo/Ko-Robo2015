41ac856d76f782804278e3665cd43950991d718d
KoiShin <koikeshinnosuke0415@gmail.com>
2015-11-10 17:12:07 +0900

    refactor : instantiate ultrasonic by array

--- Robocup/main2016/ultrasonic/main.cpp
+++ Robocup/main2016/ultrasonic/main.cpp
@@ -5,32 +5,13 @@ Ultrasonic ultrasonic_front(dp1, dp2);
 Ultrasonic ultrasonic_right(dp4, dp6);
 Ultrasonic ultrasonic_back(dp9, dp10);
 Ultrasonic ultrasonic_left(dp11, dp13);
+Ultrasonic ultrasonics[4] = {ultrasonic_front, ultrasonic_right, ultrasonic_back, ultrasonic_left};
 
 I2CSlave i2c(dp5, dp27);
 
-char return_each_distance(int sensor_pos) {
-    unsigned char distance;
-    switch (sensor_pos) {
-        case 0:
-            distance = ultrasonic_front.measure_distance();
-            break;
-        case 1:
-            distance = ultrasonic_right.measure_distance();
-            break;
-        case 2:
-            distance = ultrasonic_back.measure_distance();
-            break;
-        case 3:
-            distance = ultrasonic_left.measure_distance();
-            break;
-    }
-
-    return distance;
-}
-
 int main(void) {
     char distances[4];
-    unsigned int sensor_pos = 0;
+    unsigned int sensor_direction = 0;
     i2c.address(0xF0);
     while (1) {
         int reception_check = i2c.receive();
@@ -47,8 +28,8 @@ int main(void) {
                 break;
             case I2CSlave::NoData:
             default:
-                distances[sensor_pos % 4] = return_each_distance(sensor_pos % 4);
-                sensor_pos++;
+                distances[sensor_direction % 4] = ultrasonics[sensor_direction % 4].measure_distance();
+                sensor_direction++;
                 break;
         }
     }
