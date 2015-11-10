47de9aa9982ddfee9237aca54471684043ae500c
KoiShin <koikeshinnosuke0415@gmail.com>
2015-11-10 20:58:29 +0900

    create main algorithm of kicker-dribbler

--- Robocup/main2016/kicker-dribbler/main.cpp
+++ Robocup/main2016/kicker-dribbler/main.cpp
@@ -1,19 +1,30 @@
 #include "mbed.h"
 #include "kicker.hpp"
-#include "dribbler.hpp"
+#include "motor.hpp"
 
-Dribbler dribbler(dp26, dp25, dp24);
+Motor dribbler(dp26, dp25, dp24);
 Kicker kicker(dp9, dp10);
 
-DigitalOut kick(dp1);
+DigitalOut info_of_holding_ball(dp1);
 
-InterruptIn hold(dp2);
+DigitalIn holding_ball(dp2);
 InterruptIn kick_order(dp4);
 
-int main(void) {
-    kick_order.rise(&kicker, &Kicker::kick);
-    hold.fall(&dribbler, &Dribbler::run);
-    while (1) {
+void kick(void) {
+    dribbler.run(0);
+    wait_ms(10);
+    kicker.kick();
+}
 
+int main(void) {
+    kick_order.rise(&kick);
+    while (1) {
+        if (holding_ball) {
+            dribbler.run(0.35);
+            info_of_holding_ball = 1;
+        } else {
+            dribbler.run(0);
+            info_of_holding_ball = 0;
+        }
     }
 }
