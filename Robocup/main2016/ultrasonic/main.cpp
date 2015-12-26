6be9e3876c4396a50e13b2dd443070dee7ce9861
KoiShin <koikeshinnosuke0415@gmail.com>
2015-12-26 14:43:34 +0900

    increase the RTOS from one to four

--- Robocup/main2016/ultrasonic/main.cpp
+++ Robocup/main2016/ultrasonic/main.cpp
@@ -3,27 +3,54 @@
 #include "rtos.h"
 
 Ultrasonic* ultrasonics[4] = {
-    new Ultrasonic(dp1, dp2),  // front
-    new Ultrasonic(dp4, dp6),  // right
-    new Ultrasonic(dp9, dp10), // back
-    new Ultrasonic(dp11, dp13) // left
+    new Ultrasonic(D1, D0), // front
+    new Ultrasonic(D2, D3), // right
+    new Ultrasonic(D6, D7), // back
+    new Ultrasonic(D8, D9)  // left
 };
 
-I2CSlave i2c(dp5, dp27);
+I2CSlave i2c(D4, D5);
 
 char distances[4];
 
-void measure_distance(void const* args) {
-    unsigned int sensor_direction = 0;
+inline void measure_distance(unsigned int sensor_direction) {
+    distances[sensor_direction] = ultrasonics[sensor_direction]->measure_distance();
+}
+
+void measure_distance0(void const* args) {
+    const unsigned int sensor_direction = 0;
     while (1) {
-        distances[sensor_direction % 4] = ultrasonics[sensor_direction % 4]->measure_distance();
-        sensor_direction++;
+        measure_distance(sensor_direction);
+    }
+}
+
+void measure_distance1(void const* args) {
+    const unsigned int sensor_direction = 1;
+    while (1) {
+        measure_distance(sensor_direction);
+    }
+}
+
+void measure_distance2(void const* args) {
+    const unsigned int sensor_direction = 2;
+    while (1) {
+        measure_distance(sensor_direction);
+    }
+}
+
+void measure_distance3(void const* args) {
+    const unsigned int sensor_direction = 3;
+    while (1) {
+        measure_distance(sensor_direction);
     }
 }
 
 int main(void) {
     i2c.address(0xF0);
-    Thread thread(measure_distance);
+    Thread thread0(measure_distance0);
+    Thread thread1(measure_distance1);
+    Thread thread2(measure_distance2);
+    Thread thread3(measure_distance3);
 
     while (1) {
         int reception_check = i2c.receive();
