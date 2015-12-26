ae0bdca8d917c0ce7652bf8b0245ea51bc76af19
KoiShin <koikeshinnosuke0415@gmail.com>
2015-12-26 12:50:18 +0900

    use RTOS to measure distance of the wall

--- Robocup/main2016/ultrasonic/main.cpp
+++ Robocup/main2016/ultrasonic/main.cpp
@@ -1,5 +1,6 @@
 #include "mbed.h"
 #include "ultrasonic.hpp"
+#include "rtos.h"
 
 Ultrasonic* ultrasonics[4] = {
     new Ultrasonic(dp1, dp2),  // front
@@ -10,10 +11,20 @@ Ultrasonic* ultrasonics[4] = {
 
 I2CSlave i2c(dp5, dp27);
 
-int main(void) {
-    char distances[4];
+char distances[4];
+
+void measure_distance(void const* args) {
     unsigned int sensor_direction = 0;
+    while (1) {
+        distances[sensor_direction % 4] = ultrasonics[sensor_direction % 4]->measure_distance();
+        sensor_direction++;
+    }
+}
+
+int main(void) {
     i2c.address(0xF0);
+    Thread thread(measure_distance);
+
     while (1) {
         int reception_check = i2c.receive();
         switch (reception_check) {
@@ -28,9 +39,8 @@ int main(void) {
                 // このマイコンに受信要求された時の処理
                 break;
             case I2CSlave::NoData:
+                break;
             default:
-                distances[sensor_direction % 4] = ultrasonics[sensor_direction % 4]->measure_distance();
-                sensor_direction++;
                 break;
         }
     }
