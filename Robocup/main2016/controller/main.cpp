d892dcec614d530f3937a80e22d4f0e35af335b3
KoiShin <koikeshinnosuke0415@gmail.com>
2016-01-12 21:41:20 +0900

    add pin assign

--- Robocup/main2016/controller/main.cpp
+++ Robocup/main2016/controller/main.cpp
@@ -1,5 +1,14 @@
 #include "mbed.h"
 
+DigitalOut sw_led2(dp1);
+DigitalOut led2(dp2);
+DigitalOut sw_led(dp13);
+DigitalOut led(dp13);
+
+BusOut leds(dp14, dp28, dp15, dp26);
+
+BusIn rotary_sw(dp24, dp18, dp17, dp25);
+
 I2CSlave communication(dp5, dp27);
 
 int main(void) {
