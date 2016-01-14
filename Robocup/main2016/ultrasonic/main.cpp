f10d724398ab2ae906a60e3a5faf73e00bee123b
KoiShin <koikeshinnosuke0415@gmail.com>
2016-01-14 13:37:08 +0900

    revise algorithm

--- Robocup/main2016/ultrasonic/main.cpp
+++ Robocup/main2016/ultrasonic/main.cpp
@@ -1,6 +1,5 @@
 #include "mbed.h"
 #include "ultrasonic.hpp"
-#include "rtos.h"
 
 Ultrasonic* ultrasonics[4] = {
     new Ultrasonic(D1, D0), // front
@@ -14,23 +13,14 @@ I2CSlave i2c(D4, D5);
 char distances[4];
 
 inline void measure_distance(unsigned int sensor_direction) {
+    sensor_direction %= 4;
     distances[sensor_direction] = ultrasonics[sensor_direction]->measure_distance();
-}
-
-void measure_distance(void const* pos) {
-    while (1) {
-        measure_distance(*(const char*)pos - '0');
-    }
 }
 
 int main(void) {
     i2c.address(0xF0);
-    Thread thread0(measure_distance, (void*)"0");
-    Thread thread1(measure_distance, (void*)"1");
-    Thread thread2(measure_distance, (void*)"2");
-    Thread thread3(measure_distance, (void*)"3");
 
-    while (1) {
+    for (int i = 0; ; i++) {
         int reception_check = i2c.receive();
         switch (reception_check) {
             case I2CSlave::ReadAddressed:
@@ -44,8 +34,8 @@ int main(void) {
                 // このマイコンに受信要求された時の処理
                 break;
             case I2CSlave::NoData:
-                break;
             default:
+                measure_distance(i);
                 break;
         }
     }
