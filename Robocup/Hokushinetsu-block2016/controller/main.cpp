005808efc08d2b16fd8c986263c749355906dd3d
KoiShin <koikeshinnosuke0415@gmail.com>
2016-01-16 09:19:48 +0900

    create a switch algorithm

--- Robocup/Hokushinetsu-block2016/controller/main.cpp
+++ Robocup/Hokushinetsu-block2016/controller/main.cpp
@@ -1,7 +1,7 @@
 #include "mbed.h"
 
-DigitalOut sw_led2(dp1);
-DigitalOut led2(dp2);
+DigitalOut start_stop_sw(dp1);
+DigitalOut start_stop_sw_led(dp2);
 DigitalOut sw_led(dp13);
 DigitalOut led(dp13);
 
@@ -11,15 +11,27 @@ BusIn rotary_sw(dp24, dp18, dp17, dp25);
 
 I2CSlave communication(dp5, dp27);
 
+bool check_start_stop_sw() {
+    if (start_stop_sw) {
+        while (start_stop_sw)
+            ;
+        return true;
+    }
+    return false;
+}
+
 int main(void) {
-    int rotary_sw_val;
+    char sw_val[2];
+
     communication.address(0x32);
     while (1) {
-        rotary_sw_val = rotary_sw ^ 0xf;
+        sw_val[0] = (char)check_start_stop_sw();
+        sw_val[1] = rotary_sw ^ 0xf;
+
         int reception_check = communication.receive();
         switch (reception_check) {
             case I2CSlave::ReadAddressed:
-                communication.write(rotary_sw_val);
+                communication.write(sw_val, 1);
                 communication.stop();
                 break;
             case I2CSlave::WriteGeneral:
