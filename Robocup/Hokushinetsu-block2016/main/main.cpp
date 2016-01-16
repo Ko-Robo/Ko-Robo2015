41737a7d01908f9d9d82e60de00015b1e9aa8d34
KoiShin <koikeshinnosuke0415@gmail.com>
2016-01-16 09:16:00 +0900

    update main

--- Robocup/Hokushinetsu-block2016/main/main.cpp
+++ Robocup/Hokushinetsu-block2016/main/main.cpp
@@ -1,33 +1,18 @@
 /**
- * @file   : main.cpp (0.1)
+ * @file   : main.cpp
  * @brief  : oversee all
  * @author : Shinnosuke KOIKE
- * @date   : 2015/07/17
+ * @date   : 2016/01/16
  */
 
 #include "mbed.h"
-#include "motor.hpp"
-#include "motor-three-wheeler.hpp"
-#include "compass.hpp"
-#include "PID.h"
-
-Motor motor1(p15, p16, p23);
-Motor motor2(p17, p18, p22);
-Motor motor3(p19, p20, p21);
-
-BusIn lines(p24, p25, p26, p29);
-
-I2C top(p9, p10);
-I2C bottom(p28, p27);
-Compass compass(p9, p10);
-
-DigitalIn ball(p11);
-DigitalOut kick(p12);
-
-InterruptIn alert(p30);
+#include "micon.hpp"
 
 int main(void) {
+    Micon::init();
     while (1) {
-
+        while (!MiconState::is_running())
+            ;
+        Micon::behaves(MiconState::get_mode());
     }
 }
