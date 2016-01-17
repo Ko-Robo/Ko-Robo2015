42f061e9e22d4fef463a2bbf4c96a2e9efccf199
KoiShin <koikeshinnosuke0415@gmail.com>
2016-01-17 21:30:17 +0900

    update controller

--- Robocup/Hokushinetsu-block2016/controller/main.cpp
+++ Robocup/Hokushinetsu-block2016/controller/main.cpp
@@ -1,12 +1,10 @@
 #include "mbed.h"
 
-DigitalOut start_stop_sw(dp1);
-DigitalOut start_stop_sw_led(dp2);
-DigitalOut sw_led(dp13);
-DigitalOut led(dp13);
-
+DigitalOut start_stop_sw(dp2);
+DigitalOut start_stop_sw_led(dp1);
+DigitalOut sw(dp13);
+DigitalOut sw_led(dp14);
 BusOut leds(dp14, dp28, dp15, dp26);
-
 BusIn rotary_sw(dp24, dp18, dp17, dp25);
 
 I2CSlave communication(dp5, dp27);
@@ -21,29 +19,39 @@ bool check_start_stop_sw() {
 }
 
 int main(void) {
-    char sw_val[2];
+    char sw_values[3];
+    char data[4];
+    bool is_transmit;
 
     communication.address(0x32);
-    while (1) {
-        sw_val[0] = (char)check_start_stop_sw();
-        sw_val[1] = rotary_sw ^ 0xf;
 
-        int reception_check = communication.receive();
-        switch (reception_check) {
-            case I2CSlave::ReadAddressed:
-                communication.write(sw_val, 1);
-                communication.stop();
-                break;
-            case I2CSlave::WriteGeneral:
-                // 全マイコンに受信要求された時の処理
-                break;
-            case I2CSlave::WriteAddressed:
-                // このマイコンに受信要求された時の処理
-                break;
-            case I2CSlave::NoData:
-                break;
-            default:
-                break;
+    while (1) {
+        is_transmit = false;
+        sw_values[0] = (char)check_start_stop_sw();
+        sw_values[1] = rotary_sw ^ 0xf;
+        sw_values[2] = sw;
+
+        while (!is_transmit) {
+            int reception_check = communication.receive();
+            switch (reception_check) {
+                case I2CSlave::ReadAddressed:
+                    communication.write(sw_values, 1);
+                    communication.stop();
+                    is_transmit = true;
+                    break;
+                case I2CSlave::WriteGeneral:
+                    communication.read(data, 4);
+                    communication.stop();
+                    break;
+                case I2CSlave::WriteAddressed:
+                    communication.read(data, 4);
+                    communication.stop();
+                    break;
+                case I2CSlave::NoData:
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
