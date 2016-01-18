eb8ab655b6654c068223fa16f181e5b93d08c510
KoiShin <koikeshinnosuke0415@gmail.com>
2016-01-18 20:45:16 +0900

    refactor

--- Robocup/Hokushinetsu-block2016/main/compass.hpp
+++ Robocup/Hokushinetsu-block2016/main/compass.hpp
@@ -14,34 +14,38 @@
 class Compass {
 public:
     Compass(PinName sda, PinName scl);
+    float measure_angle();
     float measure_angle(float compare_angle);
 
 private:
     // digital compass module
     HMC6352* hmc6352;
     float initial_angle;
+    float get_correct_angle();
 };
 
 Compass::Compass(PinName sda, PinName scl) {
     hmc6352 = new HMC6352(sda, scl);
     hmc6352->setOpMode(HMC6352_CONTINUOUS, 1, 20);
-
-    do{
+    do {
         initial_angle = hmc6352->sample() / 10.0;
-    } while(initial_angle >= 360 || initial_angle <0 );
-
+    } while (initial_angle >= 360 || initial_angle < 0);
 }
 
-float Compass::measure_angle(float compare_angle = initial_angle) {
-    float hmc = hmc6352->sample() / 10.0;
-    if(hmc - compare_angle < -180){
-        return hmc - compare_angle + 360;
-    }else if(hmc - compare_angle > 180){
-        return hmc - compare_angle - 360;
-    }else{
-        return hmc - compare_angle;
-    }
+float Compass::get_correct_angle(float difference_angle) {
+    if (difference_angle < -180)     return difference_angle + 360;
+    else if (difference_angle > 180) return difference_angle - 360;
+    else                             return difference_angle;
+}
 
+float Compass::measure_angle() {
+    float difference_angle = hmc6352->sample() / 10.0 - initial_angle;
+    return get_correct_angle();
+}
+
+float Compass::measure_angle(float compare_angle) {
+    float difference_angle = hmc6352->sample() / 10.0 - compare_angle;
+    return get_correct_angle();
 }
 
 #endif /* COMPASS_H */
